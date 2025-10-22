de5cd5861c7fb179c7f49cc337f99c0177ed9093
hossainks <shakirhossain.qa@gmail.com>
2025-10-23 01:27:18 +0600

    no chnage trigger

--- 01-basic-test-01/05-find-max-number/find-max-number.js
+++ 01-basic-test-01/05-find-max-number/find-max-number.js
@@ -1,12 +1,12 @@
-// function findMaxNumber2(number) {
-//   let max = number[0];
-//   for (let i = 0; i < number.length; i++) {
-//     if (max < number[i]) {
-//       max = number[i];
-//     }
-//   }
-//   return max;
-// }
+/* function findMaxNumber(number) {
+  let max = number[0];
+  for (let i = 0; i < number.length; i++) {
+    if (max < number[i]) {
+      max = number[i];
+    }
+  }
+  return max;
+} */
 
 function findMaxNumber(number) {
   return Math.max(...number);
