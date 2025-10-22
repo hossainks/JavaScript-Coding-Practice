1d4d6d0fdbfe5d6fcb00eb64d7be98a8b1104a0f
hossainks <shakirhossain.qa@gmail.com>
2025-10-22 21:08:32 +0600

    add untracked files

--- 01-basic-test-01/06-title-case/title-case.js
+++ 01-basic-test-01/06-title-case/title-case.js
@@ -1,9 +1,9 @@
-function titleCase1(str) {
-  return str
-    .split(" ")
-    .map((word) => word[0].toUpperCase() + word.slice(1))
-    .join(" ");
-}
+// function titleCase1(str) {
+//   return str
+//     .split(" ")
+//     .map((word) => word[0].toUpperCase() + word.slice(1))
+//     .join(" ");
+// }
 
 function titleCase(str) {
   return str
